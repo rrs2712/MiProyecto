8f6ff6108793b599ba55e2744a5844ef3c1a4ded
Raul RS <rrs2712@gmail.com>
2014-03-19 22:27:09 -0600

    segundo commit para actualizar proyecto en GIT
    Signed-off-by: Raul RS <rrs2712@gmail.com>

--- MiProyecto/src/main/java/com/grupo/App.java
+++ MiProyecto/src/main/java/com/grupo/App.java
@@ -1,27 +1,22 @@
 package com.grupo;
 
-import java.util.ArrayList;
-import java.util.List;
-
-
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-	
-	String x ="";
-	
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        
-      Matematicas ext = new MateImplExt();
-      
-      int resul = ((MateImplExt)ext).resta(10, 2);
-      System.out.println("multi=" + ((MateImplExt) ext).multiplica(5, 2));
-      
-        
-    }
+public class App {
+
+	String x = "";
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+		System.out.println("Testing update 1 in GIT");
+
+		Matematicas ext = new MateImplExt();
+
+		int resul = ((MateImplExt) ext).resta(10, 2);
+		System.out.println("multi=" + ((MateImplExt) ext).multiplica(5, 2));
+
+	}
 }
