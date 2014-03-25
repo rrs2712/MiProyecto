1e60dc0a19b9ea967c6086ead29f78fe99400b6c
Raul RS <rrs2712@gmail.com>
2014-03-25 16:53:49 -0600

    Revert "Se agrega el metodo de dividir"
    This reverts commit 0c337a265a613d5b96896aa6fd083a9f5f97ff71.

--- MiProyecto/src/main/java/com/grupo/MateImplExt.java
+++ MiProyecto/src/main/java/com/grupo/MateImplExt.java
@@ -5,9 +5,5 @@ public class MateImplExt extends MatematicasImpl  implements Matematicas{
 	public int multiplica(int a, int b){
 		return a*b;
 	}
-	
-	public double division(int a, int b){
-		return a/b;
-	}
 
 }
