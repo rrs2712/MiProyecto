5cc2a804bc524cfc8eb746879e1373511f9f4b05
Raul RS <rrs2712@gmail.com>
2014-03-25 16:55:24 -0600

    Se agrega metodo de division
    Signed-off-by: Raul RS <rrs2712@gmail.com>

--- MiProyecto/src/main/java/com/grupo/MateImplExt.java
+++ MiProyecto/src/main/java/com/grupo/MateImplExt.java
@@ -5,5 +5,9 @@ public class MateImplExt extends MatematicasImpl  implements Matematicas{
 	public int multiplica(int a, int b){
 		return a*b;
 	}
+	
+	public double division(int a, int b){
+		return a/b;
+	}
 
 }
